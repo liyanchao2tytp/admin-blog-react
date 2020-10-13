863e59bef257f0bc0571e98b6e646144fd5286e6
liyanchao2tytp <57429651+liyanchao2tytp@users.noreply.github.com>
2020-10-13 19:35:56 +0800

    修改addArticle布局，添加了路由验证，暂时有点不正确

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,7 +1,8 @@
-let ipUrl = 'http://127.0.0.1:3000/'
+let ipUrl = 'http://127.0.0.1:7001/'
 
 let servicePath = {
-  checkLogin: ipUrl + 'login',    // 检查用户名和密码 登录页面
+  checkLogin: ipUrl + 'admin/login',    // 检查用户名和密码 登录页面
+  getTypeInfo: ipUrl + 'admin/getTypeInfo', //获取文章类型
 }
 
 export default servicePath
